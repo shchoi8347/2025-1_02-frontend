f8b4bc32ce2d702e77eae96dd4e0e7cca7dc7d57
shchoi8347 <csh@duksung.ac.kr>
2025-04-08 11:46:48 +0900

    수업완료코드: 20250408(화) 02반반

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Todo from './Todo';
 import React, {useState} from "react";
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from './AddTodo';
 
 function App() {
-  const [items, setItem] = useState([
+  const [items, setItems] = useState([
     {
     id: "0",
     title: "Hello World 1",
@@ -15,12 +17,31 @@ function App() {
     }
    ]);
 
-  let todoItems = 
-    items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />);
+  const addItem = (item) => {
+    item.id = "ID-" + items.length;
+    item.done = false;
+
+    setItems([...items, item]);
+    console.log("items: ", items);
+  };
+
+  let todoItems = items.length > 0 && (
+   <Paper style={{margin: 16}}>
+    <List>
+      { items.map((item) => (
+          <Todo item={item} key={item.id} />
+      ))}
+    </List>
+   </Paper>
+   );
 
   return (
     <div className="App">
-      {todoItems}
+      <Container>
+        <AddTodo addItem={addItem} />
+        <div className='TodoList'>{todoItems}</div>
+      </Container>
+      
     </div>
   );
 }
