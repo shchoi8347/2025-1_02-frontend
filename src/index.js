1733fbfa8a920b5d6773c4d18b4f19f6704285d0
shchoi8347 <csh@duksung.ac.kr>
2025-05-01 10:20:35 +0900

    수업완료코드: 02반반 20250501(목)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
+import AppRouter from './AppRouter';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(
+  <AppRouter tab="home" />
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
